9a77af09430a8bf756bba5d97327f21998790a6d
Esron Dtamar <esron.dtamar@gmail.com>
2019-05-03 12:13:20 -0300

    initialize environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:systranca_app/routes.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
   String _initialRoute = '/';
+  await DotEnv().load('.env');
 
   runApp(
     MaterialApp(
