fb0338b3142323e3f55d775f14e649373de382d7
Esron Dtamar <esron.dtamar@gmail.com>
2019-05-05 14:40:03 -0300

    inserts base code for pin dialog

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -58,7 +58,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       iconSize: 150.0,
                       tooltip: 'Destrancar porta',
                       color: Colors.black,
-                      onPressed: () {},
+                      onPressed: () async {
+                        await _neverSatisfied();
+                      },
                     ),
                   ),
                   Padding(
@@ -77,6 +79,34 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       )
+    );
+  }
+
+  Future<void> _neverSatisfied() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Rewind and remember'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('You will never be satisfied.'),
+                Text('You\’re like me. I’m never satisfied.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Regret'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
